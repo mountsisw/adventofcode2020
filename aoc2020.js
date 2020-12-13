35db4a40d8fd7cbf38db77cf8f9a7ac37f84452c
Robert <69765920+rdreid-mss@users.noreply.github.com>
2020-12-12 23:55:11 -0800

    Day 13

--- aoc2020.js
+++ aoc2020.js
@@ -14,6 +14,7 @@ solutions.set("9", {title: "Encoding Error", part1: "Day9/Part1.html", part2: "D
 solutions.set("10", {title: "Adapter Array", part1: "Day10/Part1.html", part2: "Day10/Part2.html", bg: "Day10/Day10.png"});
 solutions.set("11", {title: "Seating System", part1: "Day11/Part1.html", part2: "Day11/Part2.html", bg: "Day11/Day11.png"});
 solutions.set("12", {title: "Rain Risk", part1: "Day12/Part1.html", part2: "Day12/Part2.html", bg: "Day12/Day12.png"});
+solutions.set("13", {title: "Shuttle Search", part1: "Day13/Part1.html", part2: "Day13/Part2.html", bg: "Day13/Day13.png"});
 
 function init()
 {
